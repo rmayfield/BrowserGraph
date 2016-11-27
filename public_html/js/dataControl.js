8e817843f4fece174c91ae07ecb765713c0b412f
rmayfield <rmayfield@team3309.org>
2016-11-26 16:06:17 -0800

    Raw Data
    Implemented view of raw data. Still need to make a better looking table.
    Need to be able to add other fields to the table.

--- public_html/js/dataControl.js
+++ public_html/js/dataControl.js
@@ -4,12 +4,51 @@
  * and open the template in the editor.
  */
 
+function hideData()
+{
+    $(dataView).css("display","none"); //Display the div
+}
+
 function showData(dataField)
 {
-    console.log(dataField);
-    $(dataView).css("display","block"); //Display the div
-    $(dataHeader).text(dataField.toString()); //Title the Data
+    var dataIndex = 1; //Data index position of the data being added
+    var currentLine="";
+    var dataFields;
     
+    if($(dataView).css('display') == 'block')
+    {
+        
+    }
+    
+    $('#dataList').empty();
+    $(dataView).css("display","block"); //Display the div
+    $(dataView).css("z-index","10"); //Bring it to the front
+
+    
+    $('#dataList').append('<tr class="headerRow"><td>Timestamp</td><td>' + dataField.toString() + ' [x]</td></tr>');
+    
+    for(var j = 0 ; j<headerFields.length ; j++)
+    {
+        if(headerFields[j]==dataField.toString()) 
+        {
+            dataIndex = j;
+            console.log("Found: " + dataField.toString() + " at index " + dataIndex.toString());
+        }
+    }
+    
+    for(var i = firstLine; i<lines.length ; i++)
+    {
+        currentLine = lines[i];
+        if(currentLine.toString().includes(",")) //Standard data line
+        {
+            dataFields = currentLine.toString().split(",");
+            $('#dataList').append('<tr class="rawData"><td>' + dataFields[0].toString() + '</td><td>' + dataFields[dataIndex].toString() + '</td></tr>');
+        }else //No commas, assume it's a note
+        {         
+            console.log("Note found: " + currentLine.toString());
+             $('#dataList').append('<tr><td class="note">' + currentLine.toString() + '</td></tr>');
+        }
+    }  
 }
 
 function prepData(dataset,target)
